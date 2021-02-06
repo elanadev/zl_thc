21a76ea5bff06114cf1cf6cd1f2d3abb29733be7
Elana Cohen-Khani <elanadev@gmail.com>
2021-02-05 16:40:21 -0800

    Update schema files to include orders and backlog

--- database/schema.js
+++ database/schema.js
@@ -9,6 +9,24 @@ const inventorySchema = new Schema({
   stock: Number,
 })
 
-const Inventory = mongoose.model('Inventory', inventorySchema);
+const orderSchema = new Schema({
+  order_id: Number,
+  requested: [{ product_id: Number, quantity: Number }],
+  outcome: {
+    shipped: [{ shipment_id: Number, total_weight: Number, contents: [Number] }],
+    notShipped: [{ product_id: Number, leftToShip: Number }]
+  }
+})
 
-module.exports = { Inventory }
+const backlogSchema = new Schema({
+  order_id: Number,
+  product_id: Number,
+  waiting_to_ship: Number,
+  resolved: Boolean,
+})
+
+const Inventory = mongoose.model('Inventory', inventorySchema);
+const Order = mongoose.model('Order', orderSchema);
+const Backlog = mongoose.model('Backlog', backlogSchema);
+
+module.exports = { Inventory, Order, Backlog }

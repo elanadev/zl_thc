26ef0a4adf7506a14029032f58dfda1c030c7e69
Elana Cohen-Khani <elanadev@gmail.com>
2021-02-06 00:31:14 -0800

    Clean up Shipment class

--- server/classes.js
+++ server/classes.js
@@ -1,6 +1,5 @@
 class Shipment {
   constructor(orderId, shipmentId) {
-    this.maxWeight = 1800
     this.weight = 0
     this.items = []
     this.orderId = orderId
@@ -8,14 +7,11 @@ class Shipment {
   }
 
   addItem(newItem) {
-    if (this.weight + newItem.weight > 1800) {
-      console.error('This drone is full')
-    } else {
       console.log({ newItem })
       this.items.push(newItem)
       this.weight += newItem.weight
-    }
   }
+
 
   getWeight() {
     console.log('running weight: ', this.weight)
